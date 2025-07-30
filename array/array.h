65505364a3a61667915898b24c6dfaf5de0d8033
j-h-o-s-e-p <jcacsire@unsa.edu.pe>
2025-07-29 20:03:54 -0500

    Adecuamos la clase array para su uso posterior

--- array/array.h
+++ array/array.h
@@ -1,30 +1,71 @@
-#ifndef ARRAY_H
-#define ARRAY_H
-#define ARRAY_ERROR(msg) do { fprintf(stderr, "Array Error: %s\n", msg); exit(EXIT_FAILURE); } while(0)
+#pragma once
+#include <cstdlib>
+#include <stdexcept>
 
-struct Node; 
-
+template<typename T>
 class Array {
 private:
-    Node** data;  
-    unsigned int size;
-    unsigned int capacity;
+    T* data;
+    size_t size;
+    size_t capacity;
+    
+    void expand() {
+        capacity = capacity == 0 ? 1 : capacity * 2;
+        T* newData = new T[capacity];
+        for (size_t i = 0; i < size; ++i) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+    }
 
 public:
-    Array(unsigned int initial_capacity = 10);
-    ~Array();
-    Array(const Array& other); 
-    Array& operator=(const Array& other); 
-
-    void push_back(Node* value);
-    Node* at(unsigned int index) const;
-    Node*& at(unsigned int index);
-    void resize(unsigned int new_capacity);
-
-    Node*& unsafe_at(unsigned int index) { return data[index]; }
-
-    unsigned int get_size() const { return size; }
-    unsigned int get_capacity() const { return capacity; }
+    Array() : data(nullptr), size(0), capacity(0) {}
+    
+    ~Array() {
+        delete[] data;
+    }
+    
+    void push_back(const T& element) {
+        if (size >= capacity) {
+            expand();
+        }
+        data[size++] = element;
+    }
+    
+    T& operator[](size_t index) {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+        return data[index];
+    }
+    
+    const T& operator[](size_t index) const {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+        return data[index];
+    }
+    
+    void resize(size_t new_size) {
+        if (new_size > capacity) {
+            capacity = new_size;
+            T* newData = new T[capacity];
+            for (size_t i = 0; i < size; ++i) {
+                newData[i] = data[i];
+            }
+            delete[] data;
+            data = newData;
+        }
+        size = new_size;
+    }
+    
+    size_t getSize() const { return size; }
+    size_t getCapacity() const { return capacity; }
+    
+    // Iteradores básicos
+    T* begin() { return data; }
+    T* end() { return data + size; }
+    const T* begin() const { return data; }
+    const T* end() const { return data + size; }
 };
-
-#endif // ARRAY_H
